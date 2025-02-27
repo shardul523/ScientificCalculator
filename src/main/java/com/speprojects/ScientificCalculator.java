e6aeeb5e1f61e26e128a51647d8e1ec7392b1aa9
Shardul Sisodiya <sisodiyashardul523@gmail.com>
2025-02-27 19:39:28 +0530

    Initial Commit

---
+++ src/main/java/com/speprojects/ScientificCalculator.java
@@ -0,0 +1,11 @@
+package com.speprojects;
+
+public class ScientificCalculator {
+    public void run() {
+        System.out.println("Scientific Calculator");
+    }
+
+    public static void main(String[] args) {
+        new ScientificCalculator().run();
+    }
+}
